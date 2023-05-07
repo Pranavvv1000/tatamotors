efd095fe26e33aeb08e0b7d42854c318e0361726
Pranavvv1000 <132485339+Pranavvv1000@users.noreply.github.com>
2023-05-07 14:24:07 +0530

    Create dm.sh

---
+++ dm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python -m pip install --upgrade pip
+pip3 install flask
+pip3 install --upgrade Flask
+pip3 install datetime
+pip3 install yfinance
+pip3 install Figure
+pip3 install matplotlib
+pip3 install gunicorn
